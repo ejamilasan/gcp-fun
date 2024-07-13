791747ed5ca8841fc3c062202ba0e8e7a646d8d0
ejamilasan <ejamilasan@gmail.com>
2024-07-12 23:25:27 -0500

    add gce terraform

---
+++ gce/main.tf
@@ -0,0 +1,46 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
+
+resource "google_compute_firewall" "allow-ssh" {
+  project     = var.project_id
+  name        = "${var.ssh_network_tag}-rule"
+  network     = "default"
+  description = "Allows ssh for a specified IP"
+
+  allow {
+    protocol  = "tcp"
+    ports     = ["22"]
+  }
+
+  target_tags = [var.ssh_network_tag]
+}
+
+resource "google_compute_instance" "vm" {
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  tags = [var.ssh_network_tag]
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+}
