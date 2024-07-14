f1eece0922c137a92267d75a92e7658f3c28f476
ejamilasan <ejamilasan@gmail.com>
2024-07-13 21:59:35 -0500

    add secrets-manager terraform

---
+++ secrets-manager/variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "project_id" {
+  type = string
+}
+
+variable "secret_name" {
+  type = string
+}
+
+variable "secret_value" {
+  type = string
+}
